0e0923bd7a1d8aa4b584a5fb58bbf25701c4e362
DenSad81 <tagil81@mail.ru>
2024-07-15 12:41:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                 {"a","device for heat dissipation"},
+                 {"bb","connection device" },
+                 {"ccc","clamping device"},
+                 {"dddd","electronic component"}
+            };
+        string wordFromUser;
+        int memoriPositionCursorLeft;
+        int memoriPositionCursorTop;
+        bool isRun = true;
+        string commandExit = "exit";
+
+        while (isRun)
+        {
+            Console.Write("Input a word (or command exit): ");
+            memoriPositionCursorLeft = Console.CursorLeft;
+            memoriPositionCursorTop = Console.CursorTop;
+            wordFromUser = Console.ReadLine();
+
+            if (dictionary.ContainsKey(wordFromUser))
+            {
+                Console.SetCursorPosition(memoriPositionCursorLeft + wordFromUser.Length, memoriPositionCursorTop);
+                Console.Write("   " + dictionary[wordFromUser]);
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.SetCursorPosition(memoriPositionCursorLeft + wordFromUser.Length, memoriPositionCursorTop);
+                Console.WriteLine("   " + "This word is not presents");
+            }
+
+            if (wordFromUser == commandExit)
+            {
+                isRun = false;
+            }
+        }
+    }
+}
